30e64b1e3cbc6e84b60a32a3da5f1cc9e06a189a
mf2810 <52747041+mf2810@users.noreply.github.com>
2021-02-24 20:27:05 +0000

    Delete run.sh

--- mktop/toluene/run.sh
+++
@@ -1,5 +0,0 @@
-perl ../mktop.pl -i toluene.pdb -o topology.top -ff opls -conect no
-mv topology.top topol.top
-bash ../fixCharges.sh topol.top
-cp ../NPT.mdp ../EQM.mdp .
-python ../pdb2gmx.py toluene.pdb topol.top
