34c5181e9abcd5f14a8305e422771857cd62d1ff
mf2810 <52747041+mf2810@users.noreply.github.com>
2021-02-24 20:18:49 +0000

    Update run.sh

--- mktop/toluene/run.sh
+++ mktop/toluene/run.sh
@@ -1,6 +1,5 @@
 perl ../mktop.pl -i toluene.pdb -o topology.top -ff opls -conect no
 mv topology.top topol.top
 bash ../fixCharges.sh topol.top
-cp ../NPT.mdp .
+cp ../NPT.mdp ../EQM.mdp .
 python ../pdb2gmx.py toluene.pdb topol.top
-
